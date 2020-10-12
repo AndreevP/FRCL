287ce8c0500fba22592d9695b883577f84e63d86
Pavel Andreev <Andreev.pk@phystech.edu>
2020-10-12 21:12:16 +0300

    Added FRCL notebook and reorganised repo

--- frcl/frcl.py
+++ frcl/frcl.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 from torch.nn import ParameterList, Parameter
 import torch.nn.functional as F
 from torch.utils.data import Subset
+from torch.distributions.multivariate_normal import MultivariateNormal
+from .quadrature import GaussHermiteQuadrature1D
+from torch.distributions.kl import kl_divergence
+
 
 def create_torch_random_gen(value):
     gen = torch.Generator()
@@ -153,6 +157,121 @@ class CLBaseline(nn.Module):
         else:
             raise Exception(
                 "Criterion {} not implemented".format(criterion))
+
+
+class FRCL(nn.Module):
+    def __init__(self, base_model, h_dim, device, sigma_prior=1):
+        super(FRCL, self).__init__()
+        self.device = device
+        self.base = base_model.to(device)
+        self.sigma_prior = sigma_prior
+        self.L = Parameter(torch.eye(h_dim), requires_grad=True).to(device)
+        self.mu = Parameter(torch.normal(0, 0.1, size=(h_dim,)), requires_grad=True).to(device)
+        self.w_distr = MultivariateNormal(self.mu, scale_tril=self.L)
+        self.w_prior = MultivariateNormal(torch.zeros(h_dim).to(device),
+                                          covariance_matrix=sigma_prior*torch.eye(h_dim).to(device))
+        self.quadr = GaussHermiteQuadrature1D().to(device)
+        self.prev_tasks_distr = [] #previous tasks as torch distributions
+        self.prev_tasks_tensors = [] #previous tasks as torch tensors
+       
+    def forward(self, x, target, N_k):
+        """
+        Return -ELBO
+        N_k = len(dataset), required for unbiased estimate through minibatch
+        """
+        elbo = 0
+        phi = self.base(x)
+        def loglik(sample): #currently hardcoded for binary classification
+            return -torch.log(1 + torch.exp(-target * sample))
+        mu = self.mu
+        cov = self.L @ self.L.T
+        means = phi @ mu
+        variances = torch.diagonal(phi @ cov @ phi.T, 0)
+        elbo += (self.quadr(loglik, means, variances)).sum()
+        elbo /= x.shape[0] #mean
+        
+        kls = 0
+        kls -= kl_divergence(self.w_distr, self.w_prior)
+
+        for i in range(len(self.prev_tasks_distr)):
+            phi_i = self.base(self.prev_tasks_tensors[i])
+            cov_i = phi_i @ phi_i.T
+            p_u = MultivariateNormal(torch.zeros(phi_i.shape[0]).to(self.device),
+                                     scale_tril=phi_i)#cov_i * self.sigma_prior)
+            kls -= kl_divergence(self.prev_tasks_distr[i], p_u)
+        elbo += kls / N_k
+
+        return -elbo 
+
+    @torch.no_grad()
+    def get_predictive(self, x, k):
+        """ Computes predictive distribution according to section 2.5
+            x - batch of data
+            k - index of task
+            Return predictive distribution q_\theta(f)
+        """
+        phi_x = self.base(x)
+        phi_z = self.base(self.prev_tasks_tensors[k])
+        k_xx = phi_x @ phi_x.T
+        k_xz = phi_x @ phi_z.T
+        k_zz = phi_z @ phi_z.T
+        k_zz_ = torch.inverse(k_zz)
+        mu_u = self.prev_tasks_distr[k].mean
+        cov_u = self.prev_tasks_distr[k].covariance_matrix
+
+        mu = phi_x @ phi_z.T @ k_zz_ @  mu_u
+        sigma = k_xx + k_xz @ k_zz_ @ (cov_u  - k_zz) @ k_zz_ @ k_xz.T
+        sigma *= torch.eye(sigma.shape[0]).to(self.device) #we are interested only 
+                                                           #in diagonal part for inference
+        return MultivariateNormal(loc=mu, covariance_matrix=sigma)
+
+    @torch.no_grad()
+    def predict(self, x, k, MC_samples=20):
+        """Compute p(y) by MC estimate from q_\theta(f)?
+        """
+        distr = self.get_predictive(x.to(self.device), k)
+
+        def likelihood(y): #currently hardcoded for binary classification
+            return 1./ (1 + torch.exp(-y))
+
+        predicted = 0
+        for i in range(MC_samples):
+            sample = distr.sample()
+            predicted += likelihood(sample)
+        predicted /= MC_samples
+        return predicted
+
+    @torch.no_grad()
+    def select_inducing(self, task_dataloader, N=100, criterion="random"):
+        """Given task dataloader compute N inducing points
+           Updates self.prev_tasks_distr and self.prev_tasks_tensors
+        """
+        if (criterion=="random"):
+            smp = torch.utils.data.DataLoader(task_dataloader.dataset,
+                                              batch_size=N,
+                                              shuffle=True)
+            X, y = next(iter(smp))
+            X = X.to(self.device)
+            phi = self.base(X)
+            mu_u = phi @ self.mu
+            L_u = phi @ self.L
+            cov = L_u @ L_u.T
+            self.prev_tasks_distr.append(MultivariateNormal(loc=mu_u,
+                                                            covariance_matrix=cov))
+            self.prev_tasks_tensors.append(X)
+            return
+            
+                
+    @torch.no_grad()
+    def detect_boundary(self, x, l_old):
+        """Given new batch x and kl divergence for previous minibatch l_old
+           compute l_new and perform statistical test
+           Returns l_new and indicator of significance (0 or 1)
+        """
+        pass
+
+            
+            
 
 if __name__ == "__main__":
     clb = CLBaseline(None, None)
---
+++ frcl/quadrature.py
@@ -0,0 +1,80 @@
+import math
+
+import numpy as np
+import torch
+from torch.nn import Module
+
+
+def _pad_with_singletons(obj, num_singletons_before=0, num_singletons_after=0):
+    """
+    Pad obj with singleton dimensions on the left and right
+
+    Example:
+        >>> x = torch.randn(10, 5)
+        >>> _pad_width_singletons(x, 2, 3).shape
+        >>> # [1, 1, 10, 5, 1, 1, 1]
+    """
+    new_shape = [1] * num_singletons_before + list(obj.shape) + [1] * num_singletons_after
+    return obj.view(*new_shape)
+
+class GaussHermiteQuadrature1D(Module):
+    """
+    Implements Gauss-Hermite quadrature for integrating a function with respect to several 1D Gaussian distributions
+    in batch mode. Within GPyTorch, this is useful primarily for computing expected log likelihoods for variational
+    inference.
+
+    This is implemented as a Module because Gauss-Hermite quadrature has a set of locations and weights that it
+    should initialize one time, but that should obey parent calls to .cuda(), .double() etc.
+    """
+
+    def __init__(self, num_locs=20):
+        super().__init__()
+        self.num_locs = num_locs
+
+        locations, weights = self._locs_and_weights(num_locs)
+
+        self.locations = locations
+        self.weights = weights
+
+    def _apply(self, fn):
+        self.locations = fn(self.locations)
+        self.weights = fn(self.weights)
+        return super(GaussHermiteQuadrature1D, self)._apply(fn)
+
+    def _locs_and_weights(self, num_locs):
+        """
+        Get locations and weights for Gauss-Hermite quadrature. Note that this is **not** intended to be used
+        externally, because it directly creates tensors with no knowledge of a device or dtype to cast to.
+
+        Instead, create a GaussHermiteQuadrature1D object and get the locations and weights from buffers.
+        """
+        locations, weights = np.polynomial.hermite.hermgauss(num_locs)
+        locations = torch.Tensor(locations)
+        weights = torch.Tensor(weights)
+        return locations, weights
+
+    def forward(self, func, means, variances):#gaussian_dists):
+        """
+        Runs Gauss-Hermite quadrature on the callable func, integrating against the Gaussian distributions specified
+        by gaussian_dists.
+
+        Args:
+            - func (callable): Function to integrate
+            - gaussian_dists (Distribution): Either a MultivariateNormal whose covariance is assumed to be diagonal
+                or a :obj:`torch.distributions.Normal`.
+        Returns:
+            - Result of integrating func against each univariate Gaussian in gaussian_dists.
+        """
+      #  means = gaussian_dists.mean
+      #  variances = gaussian_dists.variance#.detach()
+
+        locations = _pad_with_singletons(self.locations, num_singletons_before=0, num_singletons_after=means.dim())
+
+        shifted_locs = torch.sqrt(2.0 * variances) * locations + means
+        log_probs = func(shifted_locs)
+        weights = _pad_with_singletons(self.weights, num_singletons_before=0, num_singletons_after=log_probs.dim() - 1)
+
+        res = (1 / math.sqrt(math.pi)) * (log_probs * weights)
+        res = res.sum(tuple(range(self.locations.dim())))
+
+        return res
